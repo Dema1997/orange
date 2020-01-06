473ec7c82c61bca6fde3a3fae1c35dd48fcd3de4
Filippo <filippodemarco1997@yahoo.it>
2020-01-06 15:54:12 +0100

    Initial commit

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.static(__dirname + '/public'));
 
 
 // routes
-
-app.listen(8080,function(){
+var port = process.env.port||3000;
+app.listen(port,function(){
   console.log("Server (localhost) listening on port 8080..");
 });
 
